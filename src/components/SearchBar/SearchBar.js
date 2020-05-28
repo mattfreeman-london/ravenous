63fc2a04a2111710c51485cd8bf6dd89e12b69a4
Matt Freeman <mathew.s.l.freeman@gmail.com>
2020-05-28 14:51:53 +0100

    adding search bar functionality

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './SearchBar.css';
 
-const searchByOptions = {
+const sortByOptions = {
     'Best Match': 'best_match',
     'Highest Rated': 'rating',
     'Most Reviewed': 'review_count'
-}
+};
+
+class SearchBar extends React.Component {
+    renderSortByOptions() {
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption];
+        })
+    };
+};
