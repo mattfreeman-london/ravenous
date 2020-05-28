bfa3e2c7ff551da77241a2315f3a21da87298f87
Matt Freeman <mathew.s.l.freeman@gmail.com>
2020-05-28 13:46:26 +0100

    completed and exported BusinessList

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,7 +4,16 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
     render() {
-        
+        return
+            <div className="BusinessList">
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+                <Business />
+            </div>
     }
 };
 
+export default BusinessList;
