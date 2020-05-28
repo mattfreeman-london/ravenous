fcd2f6aa54dfe601af6658929ee78816d8bd0446
Matt Freeman <mathew.s.l.freeman@gmail.com>
2020-05-28 13:35:45 +0100

    add Business List folder and files

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Business.css';
 
 const business = {
     imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
@@ -39,3 +40,4 @@ class Business extends React.component {
     }
 };
 
+export default Business;
